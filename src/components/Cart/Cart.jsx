ad901bb6879eb879fb2ba20c0bee7263e34d228b
kevinmbuicode <kevinmushan@gmail.com>
2022-09-15 11:54:11 +0300

    Updated EmptyCart with link, Navbar cart display

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,58 +1,60 @@
 import { Box, Button, Container, Grid, Typography } from "@mui/material";
-import CartItem from './CartItem/CartItem';
+import CartItem from "./CartItem/CartItem";
+import { Link } from "react-router-dom";
 import React from "react";
 
 const Cart = ({ cart }) => {
-
-
   const EmptyCart = () => (
-      <>
-        <Typography variant="subtitle1">Your cart is empty, start adding some!</Typography>
-        <Button variant="contained" color="secondary">
-          Add Items
-        </Button>
-      </>
-    );
+    <>
+      <Typography variant="subtitle1">
+        Your cart is empty
+        <Link to="/" color="success" sx={{ textDecoration: "none"}}>
+          , start adding some!
+        </Link>
+      </Typography>
+    </>
+  );
 
   const FilledCart = () => (
-      <>
-        <Grid container spacing={3}>
-          {cart.line_items.map((item) => (
-            <Grid item xs={12} sm={4} key={item.id}>
-              <CartItem item={item}/>
-            </Grid>
-          ))}
-        </Grid>
-        <Box sx={{ display: "flex", justifyContent: "space-between", marginTop: '5%',flexDirection: "column"}}>
-          <Typography variant="h4">
-            Total: {cart.subtotal.formatted_with_symbol}
-          </Typography>
-          <Box sx={{ display: "flex", gap: 1}}>
-            <Button
-              variant="contained"
-              color="secondary"
-              size="small"
-            >
-              Empty Cart
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              size="small"
-            >
-              Checkout
-            </Button>
-          </Box>
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem item={item} />
+          </Grid>
+        ))}
+      </Grid>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "5%",
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h4">
+          Total: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <Box sx={{ display: "flex", gap: 1 }}>
+          <Button variant="contained" color="secondary" size="small">
+            Empty Cart
+          </Button>
+          <Button variant="contained" color="primary" size="small">
+            Checkout
+          </Button>
         </Box>
-      </>
-    );
+      </Box>
+    </>
+  );
 
   if (!cart.line_items) return "Loading...";
 
   return (
     <Container>
-      <Box sx={{ marginBottom: '5%' }} />
-      <Typography variant="h4" gutterBottom>Your Shopping Cart</Typography>
+      <Box sx={{ marginBottom: "5%" }} />
+      <Typography variant="h4" gutterBottom>
+        Your Shopping Cart 
+      </Typography>
       {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,51 @@
-import { ShoppingCart } from '@mui/icons-material';
-import { AppBar, Badge, Box, IconButton, Toolbar, Typography } from '@mui/material';
-import React from 'react';
-import Logo from '../../assets/MarketplaceLogo.png'
+import { ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import Logo from "../../assets/MarketplaceLogo.png";
 
 const Navbar = ({ totalItems }) => {
+  const location = useLocation();
   return (
     <div>
-        <AppBar position='relative' color='inherit' sx={{marginBottom: 1}}>
-            <Toolbar>
-                <Typography variant="h6" color="inherit">
-                    <img src={Logo} alt="Commerce.js" height="25px"/>
-                    Johnny's Store
-                </Typography>
-                <Box sx={{ display: 'flex', flexGrow: 1}}/>
-                <Box>
-                    <IconButton aria-label='Show cart items' color='inherit'>
-                        <Badge badgeContent={totalItems} color='secondary'>
-                            <ShoppingCart/>
-                        </Badge>
-                    </IconButton>
-                </Box>
-            </Toolbar>
-        </AppBar>
+      <AppBar position="relative" color="inherit" sx={{ marginBottom: 1 }}>
+        <Toolbar>
+          <Typography
+            component={Link}
+            to="/"
+            sx={{ textDecoration: "none" }}
+            variant="h6"
+            color="inherit"
+          >
+            <img src={Logo} alt="Commerce.js" height="25px" />
+            Johnny's Store
+          </Typography>
+          <Box sx={{ display: "flex", flexGrow: 1 }} />
+          {location.pathname === "/" && (
+            <Box>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </Box>
+          )}
+        </Toolbar>
+      </AppBar>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
