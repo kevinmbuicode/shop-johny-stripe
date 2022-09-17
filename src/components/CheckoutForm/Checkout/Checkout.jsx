e16d7a11e39b4fa7145cc85803ddd94f869d218d
kevinmbuicode <kevinmushan@gmail.com>
2022-09-17 10:27:50 +0300

    Adjusted checkout

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,49 +1,163 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Typography,
-  Paper,
-  Stepper,
-  StepLabel,
-  Step,
-} from "@mui/material";
-import PaymentForm from '../PaymentForm';
+// import React, { useState } from "react";
+// import {
+//   Box,
+//   Typography,
+//   Paper,
+//   Stepper,
+//   StepLabel,
+//   Step,
+// } from "@mui/material";
+// import PaymentForm from '../PaymentForm';
+// import AddressForm from '../AddressForm';
+
+// const steps = ["Shipping address", "Payment details"];
+
+// const Checkout = () => {
+//   const [activeStep, setActiveStep] = useState(0);
+
+//   // Determine which form to render based on the step we currenmtly in
+//   const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+
+//   const Confirmation = () => (
+//     <div>
+//       Confirmation
+//     </div>
+//   )
+
+//   return (
+//     <>
+//       <Box />
+//       <main>
+//         <Paper>
+//           <Typography variant="h4" align="center">
+//             Checkout
+//           </Typography>
+//           <Stepper activeStep={activeStep}>
+//             {steps.map((step) => (
+//               <Step key={step}>
+//                 <StepLabel>{step}</StepLabel>
+//               </Step>
+//             ))}
+//           </Stepper>
+//           {activeStep === steps.length ? <Confirmation/> : <Form/>}
+//         </Paper>
+//       </main>
+//     </>
+//   );
+// };
+
+// export default Checkout;
+
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Paper from '@mui/material/Paper';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
+import Review from '../Review';
 
-const steps = ["Shipping address", "Payment details"];
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" to='/'>
+        Johny's store
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-const Checkout = () => {
-  const [activeStep, setActiveStep] = useState(0);
+const steps = ['Shipping address', 'Payment details', 'Review your order'];
 
-  // Determine which form to render based on the step we currenmtly in
-  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+      return <PaymentForm />;
+    case 2:
+      return <Review />;
+    default:
+      throw new Error('Unknown step');
+  }
+}
 
-  const Confirmation = () => (
-    <div>
-      Confirmation
-    </div>
-  )
+const theme = createTheme();
+
+export default function Checkout() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
 
   return (
-    <>
-      <Box />
-      <main>
-        <Paper>
-          <Typography variant="h4" align="center">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <Typography component="h1" variant="h4" align="center">
             Checkout
           </Typography>
-          <Stepper activeStep={activeStep}>
-            {steps.map((step) => (
-              <Step key={step}>
-                <StepLabel>{step}</StepLabel>
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
               </Step>
             ))}
           </Stepper>
-          {activeStep === steps.length ? <Confirmation/> : <Form/>}
-        </Paper>
-      </main>
-    </>
-  );
-};
+          <React.Fragment>
+            {activeStep === steps.length ? (
+              <React.Fragment>
+                <Typography variant="h5" gutterBottom>
+                  Thank you for your order.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Your order number is #2001539. We have emailed your order
+                  confirmation, and will send you an update when your order has
+                  shipped.
+                </Typography>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {getStepContent(activeStep)}
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                  {activeStep !== 0 && (
+                    <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                      Back
+                    </Button>
+                  )}
 
-export default Checkout;
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 3, ml: 1 }}
+                  >
+                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                  </Button>
+                </Box>
+              </React.Fragment>
+            )}
+          </React.Fragment>
+        </Paper>
+        <Copyright />
+      </Container>
+    </ThemeProvider>
+  );
+}
