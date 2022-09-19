2253be8b6a46cc867214039df52de4a24936acf4
kevinmbuicode <kevinmushan@gmail.com>
2022-09-19 08:31:23 +0300

    Remove function working sucessfully, App done

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
               />
             }
           />
-          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout" element={<Checkout cart={cart}/>} />
         </Routes>
       </div>
     </BrowserRouter>
